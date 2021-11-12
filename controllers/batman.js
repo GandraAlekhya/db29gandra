66423b4da768668361b6d0c49534316fa8fe60a2
Alekhya Gandra <alekhya.rachu@gmail.com>
2021-11-12 00:36:12 -0600

    final

--- controllers/batman.js
+++ controllers/batman.js
@@ -3,8 +3,15 @@ var batman = require('../models/batman');
 
  
 // for a specific batman. 
-exports.batman_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: batman detail: ' + req.params.id); 
+exports.batman_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await batman.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
  
 // Handle batman create on POST. 
@@ -34,9 +41,25 @@ exports.batman_delete = function(req, res) {
 }; 
  
 // Handle batman update form on PUT. 
-exports.batman_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: batman update PUT' + req.params.id); 
-};
+exports.batman_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await batman.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.actor)  
+               toUpdate.actor = req.body.actor; 
+        if(req.body.age) toUpdate.age = req.body.age; 
+        if(req.body.color) toUpdate.color = req.body.color; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
 
 // List of all batmans 
 exports.batman_list = async function(req, res) { 
